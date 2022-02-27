61fc9d3414a231860c37db74f576259683223001
Ali Aboshady <ali_aboshady@hotmail.com>
2022-02-27 12:11:17 +0000

    Initial commit - added initial buttons and tooltip

---
+++ Assets/Scripts/InteractiveButton.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class InteractiveButton : MonoBehaviour
+{
+    [SerializeField] Tooltip tooltip;
+}
---
+++ Assets/Scripts/Tooltip.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tooltip : MonoBehaviour
+{
+    [SerializeField] Text textObject;
+    [SerializeField] string content;
+}
