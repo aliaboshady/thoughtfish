9e940aa2debc3c406ceceb0f08d8d2803b7e642c
Ali Aboshady <ali_aboshady@hotmail.com>
2022-02-27 15:08:37 +0000

    Mouse actions are recordable

--- Assets/Scripts/MouseRecord.cs
+++ Assets/Scripts/MouseRecord.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public struct MouseRecord
 {
     public Vector3 MousePosition;
-    public bool bIsLeftClicking;
-    public bool bIsRightClicking;
+    public bool bIsLeftClickingDown;
+    public bool bIsLeftClickingUp;
+    public bool bIsRightClickingDown;
+    public bool bIsRightClickingUp;
 }
--- Assets/Scripts/MouseReplaySystem.cs
+++ Assets/Scripts/MouseReplaySystem.cs
@@ -1,12 +1,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Runtime.InteropServices;
-using System.Windows;
 
 public class MouseReplaySystem : MonoBehaviour
 {
 	[DllImport("user32.dll")]
 	static extern bool SetCursorPos(int X, int Y);
+
+	[DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+	public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+
+	private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+	private const int MOUSEEVENTF_LEFTUP = 0x04;
+	private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+	private const int MOUSEEVENTF_RIGHTUP = 0x10;
 
 	private List<MouseRecord> mouseRecords;
 	bool isInRecordMode;
@@ -24,10 +31,6 @@ public class MouseReplaySystem : MonoBehaviour
 	private void Update()
 	{
 		HandleRecordReplayInput();
-	}
-
-	private void FixedUpdate()
-	{
 		if (isInRecordMode) Record();
 		else if (isInReplayMode) Replay();
 	}
@@ -49,14 +52,20 @@ public class MouseReplaySystem : MonoBehaviour
 		{
 			Cancel();
 		}
+		if (Input.GetKey(KeyCode.C)) // Clear
+		{
+			mouseRecords.Clear();
+		}
 	}
 
 	void Record()
 	{
 		MouseRecord NewMouseRecord = new MouseRecord();
 		NewMouseRecord.MousePosition = Input.mousePosition;
-		NewMouseRecord.bIsLeftClicking = Input.GetMouseButton(0);
-		NewMouseRecord.bIsRightClicking = Input.GetMouseButton(1);
+		NewMouseRecord.bIsLeftClickingDown = Input.GetMouseButtonDown(0);
+		NewMouseRecord.bIsLeftClickingUp = Input.GetMouseButtonUp(0);
+		NewMouseRecord.bIsRightClickingDown = Input.GetMouseButtonDown(1);
+		NewMouseRecord.bIsRightClickingUp = Input.GetMouseButtonUp(1);
 		mouseRecords.Add(NewMouseRecord);
 	}
 
@@ -69,18 +78,21 @@ public class MouseReplaySystem : MonoBehaviour
 		int mouseY = (int)mouseRecord.MousePosition.y;
 
 		SetCursorPos(mouseX, monitorResolution.height - mouseY);
-		if(++replayIndex >= mouseRecords.Count)
+		if(mouseRecord.bIsLeftClickingDown) mouse_event(MOUSEEVENTF_LEFTDOWN, (uint)mouseX, (uint)mouseY, 0, 0);
+		if(mouseRecord.bIsLeftClickingUp) mouse_event(MOUSEEVENTF_LEFTUP, (uint)mouseX, (uint)mouseY, 0, 0);
+		if(mouseRecord.bIsRightClickingDown) mouse_event(MOUSEEVENTF_RIGHTDOWN, (uint)mouseX, (uint)mouseY, 0, 0);
+		if(mouseRecord.bIsRightClickingUp) mouse_event(MOUSEEVENTF_RIGHTUP, (uint)mouseX, (uint)mouseY, 0, 0);
+
+		if (++replayIndex >= mouseRecords.Count)
 		{
 			Cancel();
 		}
-		
 	}
 
 	void Cancel()
 	{
 		isInRecordMode = false;
 		isInReplayMode = false;
-		mouseRecords.Clear();
 		replayIndex = 0;
 	}
 }
